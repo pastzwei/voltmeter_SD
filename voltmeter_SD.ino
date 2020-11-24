66f971a3780938fe8f44e22e0355551f1b612f83
pastzwei <pastzwei@gmail.com>
2020-11-24 15:35:16 +0900

    論文掲載にあたり動作確認。実践後にソース改善のため一度コメントアウトしたLチカ部分を復活。

--- voltmeter_SD.ino
+++ voltmeter_SD.ino
@@ -105,11 +105,12 @@ void loop()
   digitalWrite(LED_BUILTIN, HIGH);
   delay(5000);
   } 
-  
+
   //全部終わったら1回チカっとして通し番号をインクリメント
-//  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-//  delay(100);                       // wait for a second
-//  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(100);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+
   number++;
 
   //WAIT_TIMEまで待機
